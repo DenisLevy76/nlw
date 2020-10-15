856827ef8bd6cc9016c4fbac8da4f0c25eb3aa1e
Denis Levy de M. Cravo <denis.levi11@gmail.com>
2020-10-15 17:21:45 -0300

    First-commit

---
+++ src/database/fakedb.js
@@ -0,0 +1,44 @@
+module.exports = [
+   {
+      id: 1,
+      latlng: {
+         lat: "-1.3461205",
+         lng: "-48.4257475"
+      },
+      name: "Novo Espaço Acolher",
+      about: "Presta assistência a crianças de 07 a 18 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+      whatsapp: "5591985461254",
+      images: [
+         "http://127.0.0.1:5500/images/home.png",
+         "https://images.unsplash.com/photo-1585338927000-1c787b17eb5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1582167371270-68a4c033e732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1600720642653-529b16872835?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1572247324584-a841c32b95ac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1573261524391-266433971be1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+      ],
+      instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+      openingHour: "8h até 18h",
+      openOnWeekend: "1"
+   },
+   {
+      id: 2,
+      name: "Velho Espaço Acolher",
+      about: "Presta assistência a crianças de 07 a 18 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+      latlng: {
+         lat: "-1.4574195607639355",
+         lng: "-48.46824645996094"
+      },
+      whatsapp: "5591985461254",
+      images: [
+         "http://127.0.0.1:5500/images/home.png",
+         "https://images.unsplash.com/photo-1585338927000-1c787b17eb5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1582167371270-68a4c033e732?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1600720642653-529b16872835?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1572247324584-a841c32b95ac?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+         "https://images.unsplash.com/photo-1573261524391-266433971be1?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+      ],
+      instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+      openHour: "8h até 18h",
+      openOnWeekend: "0"
+   }
+]
